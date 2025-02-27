3d80b104b9cbc6bb46373d48ccacd86bf19306f9
Fatma Ahmed <fatmaahmedezz@gmail.com>
2025-02-27 18:34:39 -0500

    Create SearchBar.js

---
+++ recipe-sharing-app/src/components/SearchBar.js
@@ -0,0 +1,23 @@
+// src/components/SearchBar.js
+import React from 'react';
+import { useRecipeStore } from './recipeStore';
+
+const SearchBar = () => {
+  const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
+  const filterRecipes = useRecipeStore(state => state.filterRecipes);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    filterRecipes();
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder="Search recipes..."
+      onChange={handleSearch}
+    />
+  );
+};
+
+export default SearchBar;
