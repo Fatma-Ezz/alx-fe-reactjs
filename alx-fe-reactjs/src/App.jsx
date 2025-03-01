e99044e55c2f4cecc1d30592cdb1324581d8b00c
Fatma Ahmed <fatmaahmedezz@gmail.com>
2025-03-01 19:43:53 +0200

    Add files via upload

---
+++ alx-fe-reactjs/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <Switch>
+          <Route path="/" exact>
+            <AddRecipeForm />
+            <RecipeList />
+          </Route>
+          <Route path="/recipe/:id" component={RecipeDetails} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
