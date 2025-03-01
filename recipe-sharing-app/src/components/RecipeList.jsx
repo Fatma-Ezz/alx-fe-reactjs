1b9bf08e01014d5f36f0ec45969000c2d6a53234
Fatma Ahmed <fatmaahmedezz@gmail.com>
2025-03-01 17:48:28 -0500

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,22 @@
-import { useRecipeStore } from './recipeStore';
+import React from 'react';
+import { useRecipeStore } from '../recipeStore';
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes);  // Get filtered recipes from Zustand store
 
   return (
     <div>
-      {filteredRecipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      {filteredRecipes.length > 0 ? (
+        filteredRecipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+            {/* Render other recipe details */}
+          </div>
+        ))
+      ) : (
+        <p>No recipes found.</p>
+      )}
     </div>
   );
 };
