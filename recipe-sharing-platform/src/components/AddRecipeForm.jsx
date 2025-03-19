dae7e299a101d69fba8d4fbfdcbf500c1248fcc3
Fatma Ahmed <fatmaahmedezz@gmail.com>
2025-03-18 22:49:19 -0400

    Update AddRecipeForm.jsx

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -40,6 +40,7 @@ const AddRecipeForm = () => {
         <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded">Submit</button>
       </form>
     </div>
+    "validate", "errors", "setErrors"
   );
 };
 
